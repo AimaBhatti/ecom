a1fe618d980551d100427e34b99b93fed70c6e0e
Aima Bhatti <aimaadnan00@gmail.com>
2022-11-18 14:42:16 +0500

    no

--- src/api/order-detail/controllers/order-detail.js
+++ src/api/order-detail/controllers/order-detail.js
@@ -6,4 +6,4 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order-detail.order-detail');
+module.exports = createCoreController("api::order-detail.order-detail");
