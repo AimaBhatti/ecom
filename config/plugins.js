505984238c61fb4dcbd374f1c1f08b7ea6eaf6f3
Aima Bhatti <aimaadnan00@gmail.com>
2022-12-07 22:46:34 +0500

    commit

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,33 @@
 module.exports = ({ env }) => ({
- 
-    email: {
-      config: {
-        provider: 'sendgrid',
-        providerOptions: {
-          apiKey: env('SENDGRID_API_KEY'),
-        },
-        settings: {
-          defaultFrom: 'baggage820@gmail.com',
-          defaultReplyTo: 'baggage820@gmail.com',
-        },
+  
+  // SENDGRID
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "baggage820@gmail.com",
+        defaultReplyTo: "baggage820@gmail.com",
       },
     },
-  });
+  },
+
+  // CLOUDINARY
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
--- src/api/email/content-types/email/lifecycles.js
+++ src/api/email/content-types/email/lifecycles.js
@@ -1,16 +1,31 @@
 module.exports = {
-    async afterCreate(event){
-        const {result} = event
-        try{
-            await strapi.plugins["email"].services.email.send({
-              to: `${user.email}`,
-              subject: "Account confirmation",
-              text: `Hello ${user.username}`,
-              html: "<p>Thank you for registering!</p><p>You have to confirm your email address. Please click on the link below.</p><p><%= URL %>?confirmation=<%= CODE %></p><p>Thanks.</p>",
-            });
-        }
-        catch(e){
-            console.log(e);
-        }
+  async afterCreate(event) {
+    const { result } = event;
+    let html ='<>Something</>'
+    let subject = "No Subject"
+      
+    if (result.type === "confirmation") {
+       subject = "Account confirmation";
+       html =
+         "<p>Thank you for registering!</p><p>You have to confirm your email address. Please click on the link below.</p><p><%= URL %>?confirmation=<%= CODE %></p><p>Thanks.</p>";
     }
-}
+    else if (result.type === "order") {
+      subject = "Order";
+      html = "<p>Thank you for trusting us and placing off an order. Your order will be delivered within 7 - 8 days.</p>"
+    }
+    else if (result.type === "new-order") {
+      subject = "New Order";
+      html = "<p>A new order has been posted by a customer!</p>";
+    }
+      try {
+        await strapi.plugins["email"].services.email.send({
+          to: `${result.email}`,
+          subject,
+          text: `Hello ${result.email}`,
+          html,
+        });
+      } catch (e) {
+        console.log(e);
+      }
+  },
+};
